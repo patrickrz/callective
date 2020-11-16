636c1fd87a3a830b10bcb033c57ad9b66a1b3f87
Kanu Grover <grover.kanu@berkeley.edu>
2020-11-15 21:51:11 -0800

    finish table searchview

---
+++ MDBProject3/MDBProject3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MDBProject3
+//
+//  Created by Kanu Grover on 10/17/20.
+//  Copyright © 2020 Kanu Grover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
